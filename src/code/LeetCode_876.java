123ec1ab500b702ee30c941f6a61bbadc9662e58
youngwoolee <leeyw910205@gmail.com>
2018-11-18 16:47:04 +0900

    LeetCode 876 문제 풀이

---
+++ src/code/LeetCode_876.java
@@ -0,0 +1,49 @@
+package code;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class LeetCode_876 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public ListNode middleNode(ListNode head) {
+
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+        LeetCode_876 leetCode_876 = new LeetCode_876();
+
+        ListNode listNode1 = new ListNode(1);
+        ListNode listNode2 = new ListNode(2);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(6);
+
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+        ListNode result = leetCode_876.middleNode(listNode1);
+    }
+}
+
