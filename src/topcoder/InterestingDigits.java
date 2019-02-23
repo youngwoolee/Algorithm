efb3bc4aad7f1233400e4a4364af0765129665fa
Yongwoo, Lee <leeyw910205@gmail.com>
2019-02-23 23:27:22 +0900

    [topcoder] 전체탐색 04번 풀이

---
+++ src/topcoder/InterestingDigits.java
@@ -0,0 +1,40 @@
+package topcoder;
+
+import org.junit.Test;
+
+import java.util.Vector;
+
+public class InterestingDigits {
+    public static int[] digits(int base) {
+
+        Vector<Integer> v = new Vector<>();
+
+        for(int i=2; i< base; i++) {
+            if ((base - 1) % i == 0) {
+                v.add(i);
+            }
+        }
+
+        int[] ans = new int[v.size()];
+        for(int i=0; i<v.size(); i++) {
+            ans[i] = v.get(i);
+        }
+
+        return ans;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        int base = 10;
+        int[] answer = digits(base);
+
+
+        System.out.print("Returns:");
+        for(int i=0; i< answer.length; i++) {
+            System.out.print(answer[i] + ", ");
+        }
+    }
+}
+
