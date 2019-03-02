20d1676e1c78b6b669bf71ad7ddca9bf799a6fba
Yongwoo, Lee <leeyw910205@gmail.com>
2019-03-03 00:12:10 +0900

    [topcoder] 동적계획법 02번 풀이

---
+++ src/topcoder/CorporationSalary.java
@@ -0,0 +1,52 @@
+package topcoder;
+
+//동적 계획법 -  메모화 재귀를 사용
+public class CorporationSalary {
+
+
+    private long[] salaries;
+    public long totalSalary(String[] relations) {
+
+        salaries = new long[relations.length];
+
+        long total = 0;
+        for(int i=0; i< relations.length; i++) {
+            total += getSalary(i, relations);
+        }
+
+        return total;
+    }
+
+    private long getSalary(int i, String[] relations) {
+
+        if(salaries[i] ==0) {
+            long salary =0;
+            String relation = relations[i];
+            for(int j=0; j< relation.length(); j++) {
+                if(relation.charAt(j) =='Y') {
+                    salary += getSalary(j, relations);
+                }
+            }
+
+            if(salary ==0){
+                salary =1;
+            }
+            salaries[i] = salary;
+        }
+
+        return salaries[i];
+    }
+
+
+    public static void main(String[] args) {
+
+        CorporationSalary corporationSalary = new CorporationSalary();
+
+        String[] relations= {"NNYN", "NNYN", "NNNN", "NYYN"};
+
+        long answer = corporationSalary.totalSalary(relations);
+
+        System.out.print("Returns: " + answer);
+
+    }
+}
