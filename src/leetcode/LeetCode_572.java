3b39a3db7d4f4b6682e7ea8f85b59dd9c891d9b0
이영우 <leeyw910205@zuminternet.com>
2019-04-19 13:10:41 +0900

    [leetcode] 트리 572 풀이

---
+++ src/leetcode/LeetCode_572.java
@@ -0,0 +1,17 @@
+/* Copyright (c) 2019 ZUM Internet, Inc.
+ * All right reserved.
+ * http://www.zum.com
+ * This software is the confidential and proprietary information of ZUM
+ * , Inc. You shall not disclose such Confidential Information and
+ * shall use it only in accordance with the terms of the license agreement
+ * you entered into with ZUM.
+ *
+ * Revision History
+ * Author                    Date                     Description
+ * ------------------       --------------            ------------------
+ *   joeylee                2019-04-19
+ */
+package leetcode;
+
+public class LeetCode_572 {
+}
