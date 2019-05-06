9f9fcc6678fcb791286ee4fd62eccce99c5cbb8f
Yongwoo, Lee <leeyw910205@gmail.com>
2019-05-06 23:46:28 +0900

    [leetcode] tree 94번 풀이

---
+++ src/leetcode/LeetCode_94.java
@@ -0,0 +1,43 @@
+package leetcode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeetCode_94 {
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList< >();
+        helper(root, res);
+        return res;
+    }
+
+    public void helper(TreeNode root, List < Integer > res) {
+        if (root != null) {
+            if (root.left != null) {
+                helper(root.left, res);
+            }
+            res.add(root.val);
+            if (root.right != null) {
+                helper(root.right, res);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        LeetCode_94 leetCode_94 = new LeetCode_94();
+
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+
+        t1.right = t2;
+        t2.left = t3;
+
+        leetCode_94.inorderTraversal(t1);
+    }
+}
