88013cdcde0338351d71de3aa7d81c95b29c3f2b
Yongwoo, Lee <leeyw910205@gmail.com>
2020-11-13 01:18:28 +0900

    leetCode 49번풀이

---
+++ src/leetcode/LeetCode_49.java
@@ -0,0 +1,26 @@
+package leetcode;
+import java.util.*;
+
+public class LeetCode_49 {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList<>();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key))  {
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(s);
+        }
+
+        return new ArrayList(ans.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"}
+        groupAnagrams(strs);
+    }
+}
